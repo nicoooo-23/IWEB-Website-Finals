f1b5f562d9e8f011e2fe4c1b5b93c8b48ed5a00f
Nico <riveraent05@gmail.com>
2025-10-11 21:17:02 +0800

    adjusted js

--- Assets/main.js
+++ Assets/main.js
@@ -1,17 +1,19 @@
 // Responsive navigation menu
 document.addEventListener("DOMContentLoaded", function () {
-  const menuBtn = document.getElementById("menu-btn");
-  const navMenu = document.getElementById("nav-menu");
+    const menuBtn = document.getElementById("menu-btn");
+    const navMenu = document.getElementById("nav-menu");
 
-  menuBtn.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-  });
+    menuBtn.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+
+  // For closing automatically
+    const navLinks = navMenu.querySelectorAll("a");
+    navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
+    });
 });
 
-// For closing automatically
-const navLinks = navMenu.querySelectorAll("a");
-navLinks.forEach(link => {
-  link.addEventListener("click", () => {
-    navMenu.classList.remove("active");
-  });
-});
+
